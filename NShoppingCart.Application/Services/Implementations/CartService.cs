a9e0da4a9036f5479b237ad6812d8e41fb098e7a
ibrar talab <ibrartalabkhan@gmail.com>
2025-12-18 21:54:30 +0500

    create cartservice

---
+++ NShoppingCart.Application/Services/Implementations/CartService.cs
@@ -0,0 +1,55 @@
+using NShoppingCart.Core.Entities;
+
+public class CartService : ICartService
+{
+    private readonly ICartRepository _cartRepository;
+    private readonly IProductRepository _productRepository;
+
+    public CartService(ICartRepository cartRepository, IProductRepository productRepository)
+    {
+        _cartRepository = cartRepository;
+        _productRepository = productRepository;
+    }
+
+    public async Task<Cart> GetCartAsync(Guid userId) =>
+        await _cartRepository.GetOrCreateCartByUserIdAsync(userId);
+
+    public async Task AddItemToCartAsync(Guid userId, Guid productId, int quantity)
+    {
+        var product = await _productRepository.GetProductByIdAsync(productId);
+        if (product == null || product.Stock < quantity)
+            throw new Exception("Product unavailable or insufficient stock.");
+
+        var cart = await _cartRepository.GetOrCreateCartByUserIdAsync(userId);
+        var existingItem = await _cartRepository.GetCartItemAsync(new Guid(cart.Id.ToString()), productId);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+            await _cartRepository.UpdateCartItemAsync(existingItem);
+        }
+        else
+        {
+            await _cartRepository.AddItemToCartAsync(new Guid(cart.Id.ToString()), new CartItem
+            {
+                ProductId = productId,
+                Quantity = quantity
+            });
+        }
+    }
+
+    public async Task RemoveItemFromCartAsync(Guid userId, Guid productId)
+    {
+        var cart = await _cartRepository.GetCartByUserIdAsync(userId);
+        if (cart == null) return;
+
+        var item = await _cartRepository.GetCartItemAsync(new Guid(cart.Id.ToString()), productId);
+        if (item != null) await _cartRepository.DeleteCartItemAsync(new Guid(item.Id.ToString()));
+    }
+
+    public async Task ClearCartAsync(Guid userId)
+    {
+        var cart = await _cartRepository.GetCartByUserIdAsync(userId);
+        if (cart != null) await _cartRepository.ClearCartAsync(new Guid(cart.Id.ToString()));
+    }
+}
